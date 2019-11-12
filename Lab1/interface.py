518f93e608ee0a51ca396ab20bf6f2f1c58ea202
Vladislav Reentovich <vladrrv@gmail.com>
2019-11-12 21:23:49 +0300

    lab 1

---
+++ Lab1/interface.py
@@ -0,0 +1,14 @@
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+qtCreatorFile = "interface.ui"
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
+class Interface(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
--- Lab1/main.py
+++ Lab1/main.py
@@ -1,12 +1,19 @@
-import numpy as np
+import sys
+from PyQt5.QtWidgets import *
+
 from algo import algo
+from interface import Interface
 
 
 def main():
     result = algo('семейство')
     print(result)
 
+    app = QApplication(sys.argv)
+    window = Interface()
+    window.show()
+    sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
-
