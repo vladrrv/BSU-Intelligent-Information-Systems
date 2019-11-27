d05e269821883307a606a6a5572681b97f664a36
Vladislav Reentovich <vladrrv@gmail.com>
2019-11-27 21:55:38 +0300

    lab 2

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-np.random.seed(42)
+SEED = 42
+np.random.seed(SEED)
 
 
 def generate_data(n=2, num_samples=10, num_classes=3):
@@ -11,14 +13,22 @@ def generate_data(n=2, num_samples=10, num_classes=3):
     return x, y
 
 
+def load_data(path):
+    df = pd.read_csv(path)
+    cols = list(df)
+    x = df[cols[1:-1]].values
+    y = df[cols[-1]].values[..., np.newaxis]
+    return x, y
+
+
 def algorithm(x_train, y_train, x_test):
-    num_samples, n = x_train.shape
+    num_samples, num_features = x_train.shape
     num_classes = y_train.max() + 1
     mask = np.arange(num_classes) == y_train.repeat(num_classes, 1)
     m = mask.sum(axis=0)
 
     # Training
-    b = np.empty((num_classes, n))
+    b = np.empty((num_classes, num_features))
     for i in range(num_classes):
         b[i] = x_train[mask[:, i]].sum(axis=0) / m[i]
     a = np.abs(b - b.mean(axis=0))
@@ -38,17 +48,17 @@ def algorithm(x_train, y_train, x_test):
 
 
 def main():
-    x, y = generate_data(n=20, num_samples=100, num_classes=4)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-    print(x_train.shape)
-    print(y_train.shape)
-    print(x_test.shape)
-    print(y_test.shape)
+    # x, y = generate_data(n=20, num_samples=100, num_classes=5)
+    x, y = load_data('dataset.csv')
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=SEED)
+    print('Train samples: ', x_train.shape[0])
+    print('Test samples: ', x_test.shape[0])
+
     y_pred = algorithm(x_train, y_train, x_test)
     score = accuracy_score(y_test, y_pred)
-    print(score)
+    print('Accuracy: ', score)
 
 
 if __name__ == "__main__":
     main()
-
